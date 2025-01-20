0b4677239a50b9e2a8a2d0590661e70a181398bf
Cauê Alves <cauealvesb4@gmail.com>
2025-01-20 20:57:07 -0300

    Criando a primeira rota

--- Node/src/index.js
+++ Node/src/index.js
@@ -1,8 +1,18 @@
 const http = require('http');
+const users = require('../src/users')
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, { 'content-type': 'text/html' })
-    response.end('<h1>Hello Word</h1>')
+    console.log(`Method: ${request.method} | endpoint: ${request.url}`)
+
+    if(request.method === 'GET' && request.url === '/users') {
+        response.writeHead(200, { 'Content-Type': 'application/json' })
+        response.end(JSON.stringify(users))
+    } else {
+        response.writeHead(404, { 'Content-Type': 'text/html' });
+        response.end(`Cannot ${request.method} ${request.url}`)
+    }
+
+    
 })
 
 server.listen(2006, () => console.log('Servidor ouvindo na porta http://localhost:2006'))
---
+++ Node/src/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        id: 1,
+        Name: 'Cauê'
+    },
+    {
+        id: 2,
+        Name: 'Geovanna'
+    },
+    {
+        id: 3,
+        Name: 'Cleide'
+    },
+    {
+        id: 4,
+        Name: 'Inaldo'
+    }
+]
