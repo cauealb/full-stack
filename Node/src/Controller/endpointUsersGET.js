38cbc79428e24b2a39ab2ff4c78122e645852e7f
Cauê Alves <cauealvesb4@gmail.com>
2025-01-21 07:04:31 -0300

    Criando uma função para retornar as respostas

--- Node/src/Controller/endpointUsersGET.js
+++ Node/src/Controller/endpointUsersGET.js
@@ -13,30 +13,19 @@ module.exports = {
             return a.id > b.id ? 1 : -1;
         })
 
-        response.writeHead(200, { 'Content-Type': 'application/json' }),
-        response.end(JSON.stringify(arrayUsers))
+        response.send(200, arrayUsers)
     },
 
     getUserByid(request, response){
         const { id } = request.params
 
         const user = users.find((item) => item.id === Number(id))
-        console.log(user)
-        // console.log
 
         if(user) {
-            response.writeHead(200, { 'Content-Type': 'application/json' });
-            response.end(JSON.stringify(user))
-        } else {
-            response.writeHead(404, { 'Content-Type': 'application/json'});
-            response.end(JSON.stringify({ error: 'Cannot find user' }))
-        }
-    
-        // if(user) {
-        // } else {
-        //     response.writeHead(404, { 'Content-Type': 'application/json' });
-        //     response.end({ error: 'Cannot find user' })
-        // }
+            return response.send(200, user)
+        } 
+
+        response.send(404, { error: 'Cannot find user' })
 
     }
 }
