3e9f24183c2ac91663673cdf6d2edbac2283aa13
Cauê Alves <cauealvesb4@gmail.com>
2025-01-23 20:09:02 -0300

    Criando a função para as queries

--- MyContacts/src/database/index.js
+++ MyContacts/src/database/index.js
@@ -1,23 +1,16 @@
 const { Client } = require('pg')
 
-const client = new Client({
+const dt = new Client({
     host: 'localhost',
     port: 5432,
     user: 'root',
     password: 'root',
-    dataclient: 'mycontacts',
+    database: 'mycontacts',
 })
 
-client.connect()
+dt.connect()
 
-// exports.module = async (query) => {
-//     const { rows } = await base.query(query)
-//     return rows
-// }
-
-async function Query(query) {
-    const { rows } = await client.query(query)
+exports.module = async (query) => {
+    const { rows } = await dt.query(query);
     return rows;
 }
-
-Query('SELECT * FROM category').then((item) => console.log(item))
