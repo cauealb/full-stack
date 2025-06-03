60c17075195fd630e166017bcfb7fef9d862c6e4
Cauê Alves <cauealvesb4@gmail.com>
2025-06-02 23:51:30 -0300

    Criando o bundle

--- React/build/index.js
+++ React/build/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var _react = _interopRequireDefault(require("react"));
-var _reactDom = _interopRequireDefault(require("react-dom"));
+var _client = _interopRequireDefault(require("react-dom/client"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-_reactDom["default"].render(/*#__PURE__*/_react["default"].createElement("h1", null, "Ol\xE1 Mundo!"), document.getElementById('root'));
+var root = _client["default"].createRoot(document.getElementById('root'));
+root.render(/*#__PURE__*/_react["default"].createElement("h1", null, "Ol\xE1 React"));
