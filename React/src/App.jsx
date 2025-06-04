b4d0ee391f7eb08e51b245d5ac34c693deff323d
Cauê Alves <cauealvesb4@gmail.com>
2025-06-03 23:33:46 -0300

    Usando fragments em propriedades

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 const category = 'Post da semana'
 
 export default function App() {
     return (
-        <div>
+        <Fragment att="ola">
             <h1>JStack's Blog</h1>
             <span>{category}</span>
-        </div>
+        </Fragment>
     )
 }
