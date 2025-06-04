cf4e2b8d4d7db26fb1f3f551729d9d33c420147b
Cauê Alves <cauealvesb4@gmail.com>
2025-06-03 23:01:03 -0300

    short sintaxe fragmente

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -2,9 +2,9 @@ import React, { Fragment } from "react";
 
 export default function App() {
     return (
-        <Fragment>
+        <>
             <h1>Hello React</h1>
             <span>Hello Fragment</span>
-        </Fragment>
+        </>
     )
 }
