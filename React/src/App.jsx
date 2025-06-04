1597b634ca10159503cfa55fa937c9d041721673
Cauê Alves <cauealvesb4@gmail.com>
2025-06-03 23:01:59 -0300

    Usando a div para agrupar elementos

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -2,9 +2,9 @@ import React, { Fragment } from "react";
 
 export default function App() {
     return (
-        <>
+        <div>
             <h1>Hello React</h1>
             <span>Hello Fragment</span>
-        </>
+        </div>
     )
 }
