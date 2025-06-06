5ef179e10ad4f8c6e1bf430ccac882d7f223ecd5
Cauê Alves <cauealvesb4@gmail.com>
2025-06-05 23:48:45 -0300

    Colocando os conteúdo no header

--- React/src/Header.jsx
+++ React/src/Header.jsx
@@ -3,7 +3,8 @@ import React from "react";
 export default function Header() {
     return (
         <>
-            
+            <h1>JStack's Blog</h1>
+            <span>{category}</span>
         </>
     )
 }
