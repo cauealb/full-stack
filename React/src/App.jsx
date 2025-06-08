2bc9c1b815a2569405541e88ef5d426db9a94f21
Cauê Alves <cauealvesb4@gmail.com>
2025-06-08 12:53:27 -0300

    Criando a estrura versão correta com imutabilidade

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -13,17 +13,22 @@ const post = [
 ]
 
 export default function App() {
-    function handleNewPost() {
-        post.push(
-            {
-                id:  Math.random(), 
-                title: `Título do post ${post.length + 1}`, 
-                subtitle: `Subtitle do post ${post.length + 1}`, 
-                likes: 50
-            }
-        )
+    const [newPost, setNewPost] = useState([
+    {id:  Math.random(), title: "Título do post 1", subtitle: "Subtitle do Post 1", likes: 10},
+    {id:  Math.random(), title: "Título do post 2", subtitle: "Subtitle do Post 2", likes: 5},
+    {id:  Math.random(), title: "Título do post 3", subtitle: "Subtitle do Post 3", likes: 10},
+    {id:  Math.random(), title: "Título do post 4", subtitle: "Subtitle do Post 4", likes: 12},
+    {id:  Math.random(), title: "Título do post 5", subtitle: "Subtitle do Post 5", likes: 27},
+])
 
-        console.log(post)
+    function handleNewPost() {
+        const newPost = {
+            id:  Math.random(), 
+            title: "Título do post 1", 
+            subtitle: "Subtitle do Post 1", 
+            likes: 10
+        }
+
     }
 
 
