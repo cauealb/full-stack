7f1360e796947f4c6635d34dbfca3b0a0b1c0787
Cauê Alves <cauealvesb4@gmail.com>
2025-06-08 12:57:33 -0300

    Usando imutabilidade e criando um novo post como correto no React

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -4,16 +4,8 @@ import Header from "./Header.jsx";
 
 const category = 'Post da semana'
 
-const post = [
-    {id:  Math.random(), title: "Título do post 1", subtitle: "Subtitle do Post 1", likes: 10},
-    {id:  Math.random(), title: "Título do post 2", subtitle: "Subtitle do Post 2", likes: 5},
-    {id:  Math.random(), title: "Título do post 3", subtitle: "Subtitle do Post 3", likes: 10},
-    {id:  Math.random(), title: "Título do post 4", subtitle: "Subtitle do Post 4", likes: 12},
-    {id:  Math.random(), title: "Título do post 5", subtitle: "Subtitle do Post 5", likes: 27},
-]
-
 export default function App() {
-    const [newPost, setNewPost] = useState([
+    const [post, setPost] = useState([
     {id:  Math.random(), title: "Título do post 1", subtitle: "Subtitle do Post 1", likes: 10},
     {id:  Math.random(), title: "Título do post 2", subtitle: "Subtitle do Post 2", likes: 5},
     {id:  Math.random(), title: "Título do post 3", subtitle: "Subtitle do Post 3", likes: 10},
@@ -24,11 +16,12 @@ export default function App() {
     function handleNewPost() {
         const newPost = {
             id:  Math.random(), 
-            title: "Título do post 1", 
-            subtitle: "Subtitle do Post 1", 
+            title: `Título do post ${post.length + 1}`, 
+            subtitle: `Subtitle do post ${post.length + 1}`,
             likes: 10
         }
 
+        setPost([...post, newPost])
     }
 
 
