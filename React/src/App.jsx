2e7ac6e22c21fe9c9e5e3d5af54ff76619e4e0ff
Cauê Alves <cauealvesb4@gmail.com>
2025-06-08 12:47:22 -0300

    colocando o key para o React

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -5,11 +5,11 @@ import Header from "./Header.jsx";
 const category = 'Post da semana'
 
 const post = [
-    {title: "Título do post 1", subtitle: "Subtitle do Post 1", likes: 10},
-    {title: "Título do post 2", subtitle: "Subtitle do Post 2", likes: 5},
-    {title: "Título do post 3", subtitle: "Subtitle do Post 3", likes: 10},
-    {title: "Título do post 4", subtitle: "Subtitle do Post 4", likes: 12},
-    {title: "Título do post 5", subtitle: "Subtitle do Post 5", likes: 27},
+    {id:  Math.random(), title: "Título do post 1", subtitle: "Subtitle do Post 1", likes: 10},
+    {id:  Math.random(), title: "Título do post 2", subtitle: "Subtitle do Post 2", likes: 5},
+    {id:  Math.random(), title: "Título do post 3", subtitle: "Subtitle do Post 3", likes: 10},
+    {id:  Math.random(), title: "Título do post 4", subtitle: "Subtitle do Post 4", likes: 12},
+    {id:  Math.random(), title: "Título do post 5", subtitle: "Subtitle do Post 5", likes: 27},
 ]
 
 export default function App() {
@@ -23,6 +23,7 @@ export default function App() {
 
             {post.map(item => (
                 <Post 
+                    key={item.id}
                     obj={{
                         title: item.title,
                         subtitle: item.subtitle
