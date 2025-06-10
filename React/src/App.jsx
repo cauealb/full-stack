3cbd894f0d5245f2acda71971fdbb529f83dadd7
Cauê Alves <cauealvesb4@gmail.com>
2025-06-10 00:59:17 -0300

    Usandod o Context com sucesso

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -4,7 +4,7 @@ import Header from "./Header.jsx";
 
 const category = 'Post da semana'
 
-const ContextProvider = createContext('dark')
+export const ContextProvider = createContext('dark')
 
 export default function App() {
     const [post, setPost] = useState([
--- React/src/Button.jsx
+++ React/src/Button.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import { ContextProvider } from "./App.jsx";
 
 export default function Button(props) {
+    const theme = useContext(ContextProvider)
+
     return (
         <button 
             onClick={props.onClick}
+            style={{
+                color: theme === 'dark' ? "#fff" : "#000",
+                backgroundColor: theme === 'dark' ? "#000" : "#fff"
+            }}
         >
             {props.children}    
         </button>
