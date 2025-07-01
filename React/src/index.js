f2fcaefeb655789bfcb7084f78f7b234d18bb840
Cauê Alves <cauealvesb4@gmail.com>
2025-07-01 00:30:15 -0300

    Incluindo o estilo global

--- React/src/index.js
+++ React/src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDom from 'react-dom/client'
 
+import GlobalStyle from './style/index.js'
+
 import App from "./components/App.jsx";
 
 const root = ReactDom.createRoot(document.getElementById('root'))
 root.render(
-    <App />
+    <>
+        <GlobalStyle />
+        <App />
+    </>
 )
