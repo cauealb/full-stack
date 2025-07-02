d1db87e4ef0394dd81852fb9ebab42b230b40d0d
Cauê Alves <cauealvesb4@gmail.com>
2025-07-02 01:15:49 -0300

    Criando o estilo global

--- React/src/index.js
+++ React/src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDom from 'react-dom/client'
-
-import GlobalStyle from './style/index.js'
-
 import App from "./App.jsx";
 
 const root = ReactDom.createRoot(document.getElementById('root'))
---
+++ React/src/style/index.js
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalSyle = createGlobalStyle`
+    body {
+        background-color: #3c3c3c;
+        color: white;
+    }
+`;
