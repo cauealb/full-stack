85ffef009c06aa4c272544930c395c9c9dbf24a0
Cauê Alves <cauealvesb4@gmail.com>
2025-07-02 01:06:27 -0300

    Resetando o componente de ListPost

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,12 +1,10 @@
-import React, { useState } from "react";
-import Post from "./components/ListPost/Post.jsx";
-import Header from "./components/Header/index.jsx";
-import { ThemeProvider } from "./components/context/Context.jsx";
+import Layout from "./components/Layout";
 
-import { Title } from "./components/style.js";
-
-const category = "Post da semana";
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Layout />
+    </>
+  );
 }
--- React/src/components/ListPost/index.jsx
+++ React/src/components/ListPost/index.jsx
@@ -1,40 +1,8 @@
-import React, { useContext } from "react"
-import PropsTypes from 'prop-types'
-import PostHeader from "./PostHeader.jsx"
-import { Container, SubTitle } from "./style.js"
-
-
 
 export default function ListPost({obj, onRemove}) {
     return (
         <>
-            <Container removed={obj.removed}>
-                <PostHeader 
-                    obj={{
-                        id: obj.id,
-                        title: obj.title,
-                        read: obj.read
-                    }}
-                    onRemove={onRemove}
-                />
-                <br /> 
-                <br />
-                <SubTitle>{obj.subtitle}</SubTitle>
-            </Container>
-            <p>Likes: {obj.likes}</p>
-            <br />
+
         </>
     )
-}
-
-Post.propTypes = {
-    obj: PropsTypes.shape({
-        id: PropsTypes.number.isRequired,
-        title: PropsTypes.string.isRequired,
-        subtitle: PropsTypes.string.isRequired,
-        likes: PropsTypes.number.isRequired,
-        read: PropsTypes.bool.isRequired,
-        removed: PropsTypes.bool.isRequired,
-    }).isRequired,
-    onRemove: PropsTypes.func.isRequired,
 }
