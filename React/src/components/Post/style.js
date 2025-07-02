815b2a5ba3081f0efa7c71fe7858e6a2cb34e25d
Cauê Alves <cauealvesb4@gmail.com>
2025-07-01 23:43:57 -0300

    usando o context sem o css

--- React/src/components/Post/style.js
+++ React/src/components/Post/style.js
@@ -4,7 +4,7 @@ export const Container = styled.article`
   /* opacity: ${(props) => props.removed ? 0.5 : 1};
   color: ${(props) => props.removed ? '#ff0000' : '#fff'} */
 
-  ${(props) => css`
+  ${(props) => `
     opacity: ${props.removed ? 0.5 : 1};
     color: ${props.removed ? '#ff0000' : '#fff'}
   `}
