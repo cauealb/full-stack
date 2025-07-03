601bb8e7c3b20aaf8692731ff8780a347207609e
Cauê Alves <cauealvesb4@gmail.com>
2025-07-02 23:14:49 -0300

    Finalizando a estilização do Header

--- React/src/components/Header/index.jsx
+++ React/src/components/Header/index.jsx
@@ -1,11 +1,12 @@
 import React from "react"
+import Container from './style.js'
 
 export default function Header() {
 
     return (
-        <header>
+        <Container>
             <h1>JStack's Blog</h1>
             <button type="button">Mudar de tema</button>
-        </header>
+        </Container>
     )
 }
--- React/src/components/Header/style.js
+++ React/src/components/Header/style.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
 export default styled.header`
-    
+    display: flex;
+    justify-content: space-between;
+    background-color: black;
+    margin: 10px;
+    border-radius: 4px;
+    padding: 20px
 `;
