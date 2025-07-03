fb30cdcec5a29fb7d0d98e07a729b5227d930490
Cauê Alves <cauealvesb4@gmail.com>
2025-07-03 00:16:11 -0300

    Estilizando o button

--- React/src/components/Footer/style.js
+++ React/src/components/Footer/style.js
@@ -3,8 +3,17 @@ import styled from 'styled-components';
 export default styled.div`
     display: flex;
     justify-content: space-between;
+    align-items: center;
     background-color: black;
     margin: 10px;
     border-radius: 4px;
     padding: 20px;
+
+    button {
+        background-color: white;
+        border: none;
+        border-radius: 4px;
+        padding: 10px;
+        cursor: pointer;
+    }
 `;
