7532217f8305a99a801131b8cd1f3539f0fa6a60
Cauê Alves <cauealvesb4@gmail.com>
2025-07-03 00:10:32 -0300

    Esilizando campos no Post

--- React/src/components/ListPost/Post/style.js
+++ React/src/components/ListPost/Post/style.js
@@ -4,4 +4,12 @@ export default styled.div`
     background-color:rgb(71, 71, 71);
     padding: 10px;
     border-radius: 8px;
+
+    h1 {
+       font-size: 24px 
+    }
+
+    span {
+        color: #ccc
+    }
 `;
