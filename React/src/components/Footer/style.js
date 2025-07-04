52e6abd803ec6ce2f2ff6bf84e1f3fe62cf988ac
Cauê Alves <cauealvesb4@gmail.com>
2025-07-04 00:14:38 -0300

    Adicionando estilização no footer

--- React/src/components/Footer/style.js
+++ React/src/components/Footer/style.js
@@ -4,13 +4,17 @@ export default styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    background-color: black;
+    background-color: ${({ theme }) => theme.headerBackgroundColor};
     margin: 10px;
     border-radius: 4px;
     padding: 20px;
 
+    h1 {
+        color: ${({ theme }) => theme.textBackgroundColor}
+    }
+
     button {
-        background-color: white;
+        background-color: ${({ theme }) => theme.btnBackgroundColor};
         border: none;
         border-radius: 4px;
         padding: 10px;
