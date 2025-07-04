4beec9cd3b9f9b870e74fae98c24a686f245a399
Cauê Alves <cauealvesb4@gmail.com>
2025-07-03 23:38:02 -0300

    Usando o ThemeProvider do styled components

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import Layout from "./components/Layout";
 import GlobalSyle from './style/style.js'
 
+import { ThemeProvider } from "styled-components";
+import theme from "./style/theme/index.js";
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalSyle />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 }
