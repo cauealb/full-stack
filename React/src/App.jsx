fa8b20321049638cdd3dc637560c6062534af057
Cauê Alves <cauealvesb4@gmail.com>
2025-07-05 11:54:30 -0300

    Começando a troca de temas

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 
 import Layout from "./components/Layout";
 import GlobalSyle from './style/style.js'
@@ -7,6 +7,12 @@ import { ThemeProvider } from "styled-components";
 import theme from "./style/theme/index.js";
 
 export default function App() {
+  const [theme, setTheme] = useState('dark')
+
+  const currenTheme = useMemo(() => {
+    
+  }, [theme])
+
   return (
     <ThemeProvider theme={theme.light}>
       <GlobalSyle />
