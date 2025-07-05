d00fec6d3d4da2065ff168876f62fedbf4cc90f9
Cauê Alves <cauealvesb4@gmail.com>
2025-07-05 13:46:21 -0300

    Finaliando header e o teste

--- React/src/components/Header/index.jsx
+++ React/src/components/Header/index.jsx
@@ -1,12 +1,17 @@
-import React from "react"
+import React, { useContext } from "react"
 import Container from './style.js'
+import { Context } from "../Context/AppContext.jsx"
 
 export default function Header() {
+    const { handleToggleTheme, themes } = useContext(Context)
+
     return (
         <Container>
             <h1>JStack's Blog</h1>
-            <button>
-                Mudar tema 
+            <button
+                onClick={handleToggleTheme}
+            >
+                {`Mudar tema para ${themes === 'dark' ? 'light' : 'dark'}`} 
             </button>
         </Container>
     )
