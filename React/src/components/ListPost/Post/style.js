ff1c55f8e7ee52f1090d3ab78a372d034114d92e
Cauê Alves <cauealvesb4@gmail.com>
2025-07-05 13:59:32 -0300

    Finalizando os estilos do Post

--- React/src/components/ListPost/Post/style.js
+++ React/src/components/ListPost/Post/style.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 export default styled.div`
     background-color: ${({ theme }) => theme.postBackgroundColor};
-    padding: 10px;
-    border-radius: 8px;
+    padding: ${({ theme }) => theme.spacing.small};
+    border-radius: ${({ theme }) => theme.borderRadius};
 
     h1 {
-       font-size: 24px 
+       font-size: ${({ theme }) => theme.spacing.large};
     }
 
     span {
