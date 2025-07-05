559729bea525793ae490c3723746f26506179861
Cauê Alves <cauealvesb4@gmail.com>
2025-07-05 13:39:52 -0300

    Arruamando componente Header

--- React/src/components/Layout/index.jsx
+++ React/src/components/Layout/index.jsx
@@ -3,20 +3,13 @@ import React from "react";
 import Footer from "../Footer";
 import Header from "../Header";
 import ListPost from "../ListPost";
-import { useTheme } from "styled-components";
 
-export default function Layout({ onToggleTheme, themes }) {
+export default function Layout() {
   return (
     <>
-        <Header 
-          onToggleTheme={onToggleTheme} 
-          themes={themes}
-        />
+        <Header />
         <ListPost />
-        <Footer 
-          onToggleTheme={onToggleTheme} 
-          themes={themes}  
-        />
+        <Footer />
     </>
   );
 }
